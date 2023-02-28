d7fedda66b32aa9a0cb5bab9ea8650c31294767a
David Chin <david.w.h.chin@gmail.com>
2023-02-28 01:46:55 -0500

    Info output

--- code/slurmjob.sh
+++ code/slurmjob.sh
@@ -15,17 +15,23 @@ module load stata/mp48/17
 /bin/cp -f *.R *.do $TMP
 cd $TMP
 
+echo "SLURM_CPUS_PER_TASK = $SLURM_CPUS_PER_TASK"
+
 # generate data
+echo "Generate data ..."
 R --no-save --no-restore-data --quiet CMD BATCH 1-generate-datasets.R
 
 # do the computations in R
+echo "Do computations in R ..."
 R --no-save --no-restore-data --quiet CMD BATCH 2-benchmark-r.R
 
 # do the computations in Stata
+echo "Do computations in Stata ..."
 export STATATMP=$TMP
 stata-mp -s do 3-benchmark-stata.do
 
 # make the plots
+echo "Generate plots ..."
 R --no-save --no-restore-data --quiet CMD BATCH 4-graph.R
 
 # move script outputs and plots back, overwriting existing
